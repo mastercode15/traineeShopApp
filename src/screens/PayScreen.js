3f5459a0987a874f2033d4cc1815e2a9568a04b9
VAVR064 <vallejo064@gmail.com>
2021-09-08 17:30:47 -0500

    Añadido métodos de pago guardados y guardar nuevo método de pago

--- src/screens/PayScreen.js
+++ src/screens/PayScreen.js
@@ -1,16 +1,44 @@
 import React, {useState} from 'react';
-import { View, TextInput, StyleSheet, Text, Picker, Button, Modal, Pressable } from 'react-native';
+import { View, TextInput, StyleSheet, Text, Picker, Button, Modal, Pressable, CheckBox } from 'react-native';
 
 //https://stackoverflow.com/questions/44046037/if-else-statement-inside-jsx-reactjs
-//const savedpays = [{type : 0, foot : "1234"}];
+const savedpays = [{type : 0, bid: 0, foot : "1234"},{type : 1, bid: 0, foot : "5678"}];
+
+const savedPM = () => {
+
+  if (savedpays.length > 0){
+
+    const list = []
+
+    for (var i = 0; i < savedpays.length; i++){
+      const acc = savedpays[i];
+      const head = acc.type === 0 ? "Cuenta " : "Tarjeta ";
+      const body = "del " + (acc.bid === 0 ? "Banco A" : (acc.bid === 1 ? "Banco B" : "Banco C"));
+      const foot = " que termina en " + acc.foot;
+      list.push(<Picker.Item label={head+body+foot} value={i} />)
+    }
+
+  
+    return (
+      <View>
+        <Text>Métodos de pago guardados</Text>
+          <Picker>
+            {list}
+          </Picker>
+      </View>
+    )
+  }
+}
 
 const PayPage = () => {
     const [selectedType, setSelectedType] = useState("banks");
     const [selectedBank, setSelectedBank] = useState("bank1");
     const [modalOpen, setModalOpen] = useState(false);
     const [btnDis, setBtnDis] = useState(false);
+    const [saveCheck, setSaveCheck] = useState(false);
 
     return (
+
         <View>
         
             <Modal animationType="slide" transparent={true} visible={modalOpen} onRequestClose={() => { setModalOpen(!modalOpen); }}> 
@@ -21,13 +49,14 @@ const PayPage = () => {
                 </Pressable>
               </View>
             </Modal>
-        
-        
+
             <Text>Dirección:</Text>
             <TextInput maxLength={100} placeholder="Escriba aquí la dirección para la factura"/>
 
             <Text>Valor a pagar:</Text>
             <TextInput value = "$150.00" editable = {false} />
+
+            {savedPM()}
 
             <Text>Tipo de pago:</Text>
             <Picker selectedValue = {selectedType} onValueChange={(itemValue, itemIndex) => setSelectedType(itemValue)}>
@@ -44,6 +73,9 @@ const PayPage = () => {
 
             <Text>Ingrese su cuenta o tarjeta:</Text>
             <TextInput maxLength={16} keyboardType={'numeric'} placeholder="Ej: 1000000000 / 4500123412341234"/>
+
+            <CheckBox value={saveCheck} onValueChange={setSaveCheck} />
+            <Text>Guardar método de pago</Text>
 
             <Button disabled = {btnDis} onPress={() => {setModalOpen(!modalOpen);setBtnDis(!btnDis);}} title="Realizar pago" />
 
