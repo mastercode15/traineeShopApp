eebb110ed566dd692b0057c80ee3e7cdc878e66a
VAVR064 <vallejo064@gmail.com>
2021-09-09 17:05:52 -0500

    Rediseñada la página. Agregadas validaciones de campos

--- src/screens/PayScreen.js
+++ src/screens/PayScreen.js
@@ -3,19 +3,45 @@ import { View, TextInput, StyleSheet, Text, Picker, Modal, TouchableOpacity, Che
 import { NavigationEvents } from 'react-navigation';
 
 //Local payments saved for testing purpouses
-const savedpays = [{type : 0, bid: 0, foot : "1234"},{type : 1, bid: 0, foot : "5678"}];
+const savedpays = [{type : 0, bid: 0, foot : "1234"},
+                  {type : 1, bid: 0, foot : "5678"}];
+
 const defaultmsg = "Ingresar mensaje aquí";
+const noFillmsg = "Llene todos los campos que faltan";
+const badAcc = "Cuenta de contener 10 dígitos";
+const badCard = "Tarjeta de contener 16 dígitos";
+
+const accReg = /^\d{10}$/;
+const cardReg = /^\d{16}$/;
 
 //La aplicación en sí
 const PayPage = () => {
     const [selectedType, setSelectedType] = useState("banks");
     const [selectedBank, setSelectedBank] = useState("bank1");
     const [modalOpen, setModalOpen] = useState(false);
-    const [modalMsg, setModalMsg] = useState(defaultmsg);
     const [btnDis, setBtnDis] = useState(false);
     const [saveCheck, setSaveCheck] = useState(false);
     const [putPM, setPutPM] = useState(true);
     const [selPay, setSelPay] = useState(0);
+
+    const [usrAddr, setUsrAddr] = useState("");
+    const [usrAcc, setUsrAcc] = useState("");
+    const [modalMsg, setModalMsg] = useState(defaultmsg);
+
+    //Revisa si al ingresar un nuevo metodo de pago se han llenado los campos correctamente
+    const frontCheck = () => {
+      if (usrAddr.trim() == "" || usrAcc.trim() == ""){
+        setModalMsg(noFillmsg);
+      }
+      else if (selectedType === "banks" && !accReg.test(usrAcc.trim())){
+        setModalMsg(badAcc);
+      }
+      else if (selectedType === "cards" && !cardReg.test(usrAcc.trim())){
+        setModalMsg(badCard);
+      }
+      setBtnDis(true);
+      setModalOpen(true);
+    }
 
     //Funcion para mostar metodos de pago guardados, si esque existen
     const savedPM = () => {
@@ -34,7 +60,7 @@ const PayPage = () => {
 
         return (
           <View>
-            <Text>Métodos de pago guardados</Text>
+            <Text style={styles.labeltxt_style}>Métodos de pago guardados</Text>
               <Picker selectedValue = {selPay} onValueChange={(itemValue, itemIndex) => setSelPay(itemValue)}>
                 {list}
               </Picker>
@@ -52,36 +78,44 @@ const PayPage = () => {
 
       const savedP = []
 
-      if(savedpays.length > 0)
+      if(savedpays.length > 0){
+        //savedP.push(<br />)
         savedP.push(
           //<Button onPress={() => {setPutPM(false);}} title="Usar métodos de pago guardados"/>
           <TouchableOpacity style={!btnDis ? styles.spaybtn_style : styles.spaybtn_dis_style} disabled = {btnDis} onPress={() => {setPutPM(false);}} >
             <Text style= {styles.paytxt_style}>Métodos de pago guardados</Text>
           </TouchableOpacity>
         );
+      }
+      else{
+        savedP.push(<br />)
+      }
 
       if (putPM){
         return(
           <View>
-            <Text>Tipo de pago:</Text>
+            <Text style={styles.labeltxt_style}>Tipo de pago:</Text>
             <Picker selectedValue = {selectedType} onValueChange={(itemValue, itemIndex) => setSelectedType(itemValue)}>
                 <Picker.Item label="Transferencia Bancaria" value="banks" key="banks"/>
                 <Picker.Item label="Tarjeta Débito/Crédito" value="cards" key="cards"/>
             </Picker>
+            <br/>
 
-            <Text>Seleccione Banco/Proveedor:</Text>
+            <Text style={styles.labeltxt_style}>Seleccione Banco/Proveedor:</Text>
             <Picker selectedValue = {selectedBank} onValueChange={(itemValue, itemIndex) => setSelectedBank(itemValue)}>
               <Picker.Item label="Banco A" value="bank1" key="bank1"/>
               <Picker.Item label="Banco B" value="bank2" key="bank2"/>
               <Picker.Item label="Banco C" value="bank3" key="bank3"/>
             </Picker>
+            <br/>
 
-            <Text>Ingrese su cuenta o tarjeta:</Text>
-            <TextInput maxLength={16} keyboardType={'numeric'} placeholder="Ej: 1000000000 / 4500123412341234"/>
-            
+            <Text style={styles.labeltxt_style}>Ingrese su cuenta o tarjeta:</Text>
+            <TextInput onChangeText={(value) => setUsrAcc(value)} maxLength={16} keyboardType={'numeric'} placeholder="Ej: 1000000000 / 4500123412341234"/>
+            <br />
+
             <View style={{flexDirection:"row", alignItems:"center"}}>
               <CheckBox value={saveCheck} onValueChange={setSaveCheck}/>
-              <Text >Guardar método de pago</Text>
+              <Text >  Guardar método de pago para futuro uso</Text>
             </View>  
 
             {savedP}         
@@ -101,28 +135,31 @@ const PayPage = () => {
                     <View style={styles.modalView}>
                         <Text>{modalMsg}</Text>
                         <Text></Text> 
-                        <TouchableOpacity style={styles.modalbtn_style} onPress={() => {setModalOpen(!modalOpen);setBtnDis(!btnDis);}} >
+                        <TouchableOpacity style={styles.modalbtn_style} onPress={() => {setModalMsg(defaultmsg);setModalOpen(!modalOpen);setBtnDis(!btnDis);}} >
                             <Text style= {styles.modaltxt_style}>Entendido</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
             </Modal>
 
-            <Text>Nombre del cliente:</Text>
+            <Text style={styles.labeltxt_style}>Nombre del cliente:</Text>
             <TextInput value = "Danu Jhonson" editable = {false} />
+            <br />
+            <Text style={styles.labeltxt_style}>Dirección:</Text>
+            <TextInput onChangeText={(value) => setUsrAddr(value)} maxLength={100} placeholder="Escriba aquí la dirección para la factura"/>
+            <br />
 
-            <Text>Dirección:</Text>
-            <TextInput maxLength={100} placeholder="Escriba aquí la dirección para la factura"/>
-
-            <Text>Valor a pagar:</Text>
+            <Text style={styles.labeltxt_style}>Valor a pagar:</Text>
             <TextInput value = "$150.00" editable = {false} />
+            <br />
 
             {savedPM()}
+
 
             {newPM()}
 
 
-            <TouchableOpacity style={!btnDis ? styles.paybtn_style : styles.paybtn_dis_style} disabled = {btnDis} onPress={() => {setModalOpen(!modalOpen);setBtnDis(!btnDis);}} >
+            <TouchableOpacity style={!btnDis ? styles.paybtn_style : styles.paybtn_dis_style} disabled = {btnDis} onPress={() => {frontCheck();}} >
                 <Text style= {styles.paytxt_style}>Realizar el pago</Text>
             </TouchableOpacity>
 
@@ -188,6 +225,12 @@ const styles = StyleSheet.create({
     },
     modaltxt_style: {
       fontSize: 15,
+      color: "#FFFFFF"
+    },
+    labeltxt_style: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: "#000000"
     },
     centeredView: {
       justifyContent: "center",
