5563f6e5130cff08042abc916e00dfedf2e0a75f
Santi Rodriguez <santy95_@hotmail.com>
2021-09-09 12:33:33 -0500

    ArreglosRedirecciones

--- App.js
+++ App.js
@@ -3,21 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
 import Market from './src/screens/MarketScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-<<<<<<< HEAD
 import MonitorScreen from './src/screens/MonitorScreen';
 
 const navigator = createStackNavigator(
   {
     Home: IndexScreen,
+    Market: Market,
     Register: RegisterScreen,
     Monitor: MonitorScreen,
-=======
-const navigator = createStackNavigator(
-  {
-    Home: IndexScreen,
-    Market: Market,
-    Register: RegisterScreen
->>>>>>> 4e7676389b62ec8ba1eec78287aed4587038b22a
   },
   {
     initialRouteName: 'Home',
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -10,13 +10,12 @@ const IndexScreen = ({ navigation }) => {
                 title="Registro"
             />
             <Button
-<<<<<<< HEAD
                 onPress={() => navigation.navigate('Monitor')}
                 title="Monitor"
-=======
+            />
+            <Button
                 onPress={() => navigation.navigate('Market')}
                 title="Market"
->>>>>>> 4e7676389b62ec8ba1eec78287aed4587038b22a
             />
         </View>
     );
