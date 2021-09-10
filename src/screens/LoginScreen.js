3d914e2d83a188707d0d30930e80b22d9a951259
Rodrigo Alejandro Viteri Velez <rodrigo.viteri.velez@estudiantes.cordillera.edu.ec>
2021-09-09 23:55:15 -0500

    ya valio

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,82 +4,72 @@ import { SafeAreaView, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView
 const LoginScreen = () => {
     const [ci, setCi] = useState('');
     const [password, setPassword] = useState('');
-
-
     const handleSubmit = () => {
-        if (!ci || !password) {
-            Alert.alert("Por favor llene todos los campos para registrarse");
-        } else if (ci.length < 10) {
-            Alert.alert("Número de cédula incorrecto");
-        } else if (password < 10) {
+        if ( !ci || !password ) {
+            Alert.alert("Por favor llene todos los campos");
+        }
+        else if (ci.length < 10) {
+            Alert.alert("llenar el campo");
+        }
+        else if (password != 0 ) {
+            fetch('http://54.221.130.211:4005/clientes/login/'+  ci +"/"+ password, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
 
-            var URL = "http://34.227.98.168:4007/clientes/login/" + $.param({ id: ci, passwd: password })
-            fetch(URL, {
-                    method: 'GET',
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-
-                })
+            })
                 .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    if (data.status == undefined) {
+                .then((respuestaJson) => {
+                    alert(respuestaJson);
 
-                    } else {
-                        Alert.alert("Error de Inincio de Session");
-                    }
                 });
 
-        } else {
 
-            Alert.alert("llenar el password");
+        } else {
+            Alert.alert("llenar el campo");
         }
     }
     return (
 
+        <SafeAreaView style={styles.container} >
+            <ScrollView>
+
+                <Text style={styles.title}>Login</Text>
 
 
-        <
-        SafeAreaView style = { styles.container } >
-        <
-        ScrollView >
-        <
-        Text style = { styles.title } > Login < /Text> <
-        TextInput maxLength = { 20 }
-        style = { styles.input }
-        placeholder = "Usuario"
-        autoCapitalize = "none"
-        autoCorrect = { false }
-        value = { ci }
-        onChangeText = {
-            (newValue) => setCi(newValue)
-        }
-        />
+                <TextInput
+                    maxLength={10}
+                    style={styles.input}
+                    placeholder="Cédula"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={ci}
+                    onChangeText={(newValue) => {
+                        if (/^\d+$/.test(newValue) || newValue === '') {
+                            setCi(newValue)
+                        }
+                    }}
+                    keyboardType="phone-pad"
+                />
+                <TextInput
+                    maxLength={20}
+                    secureTextEntry={true}
+                    style={styles.input}
+                    placeholder="Contraseña"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={password}
+                    onChangeText={(newValue) => setPassword(newValue)}
+                />
+                 
+                <TouchableOpacity style={styles.loginButton} onPress={handleSubmit}>
+                    <Text style={styles.loginText}>Iniciar sesión</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </SafeAreaView >
 
-
-        <
-        TextInput maxLength = { 20 }
-        secureTextEntry = { true }
-        style = { styles.input }
-        placeholder = "Contraseña"
-        autoCapitalize = "none"
-        autoCorrect = { false }
-        value = { password }
-        onChangeText = {
-            (newValue) => setPassword(newValue)
-        }
-        />
-
-        <
-        TouchableOpacity style = { styles.loginButton }
-        onPress = { handleSubmit } >
-        <
-        Text style = { styles.loginText } > Iniciar sesión < /Text> <
-        /TouchableOpacity> <
-        /ScrollView> <
-        /SafeAreaView>
     );
 };
 
